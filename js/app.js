a0dfe0157011f115122c8099e0907036693c6903
prydt <pranoydll@gmail.com>
2017-04-18 22:40:45 -0500

    fixed parallax and added events

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-$(function(){$('.item').each(function(i){setTimeout(function(){$('.item').eq(i).addClass('visible');}, 250 * i);});});
+$(function(){$('.item').each(function(i){setTimeout(function(){$('.item').eq(i).addClass('visible');}, 200 * i);});});
